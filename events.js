ba4831697534efb761b86ebcd804bdf80f786709
Jeremy Liss <jeremyliss@gmail.com>
2017-01-17 15:30:50 +1300

    make red

--- events.js
+++ events.js
@@ -9,6 +9,7 @@ function start () {
   one()
   two()
   three()
+  four()
   // Your turn! Create a new function called `two`, then call it from here.
 }
 
@@ -35,7 +36,11 @@ function three () {
   three.addEventListener('mouseleave', makeWhite)
 }
 
-// CREATE FUNCTION four HERE
+function four () {
+  var four = document.getElementById('four')
+  four.addEventListener('click', makeRed)
+  four.addEventListener('mouseleave', makeWhite)
+}
 
 // Changes the background color of event's target
 function makeBlue (evt) {
@@ -48,6 +53,10 @@ function makeGreen (evt) {
 
 function makeOrange (evt) {
   evt.target.style.backgroundColor = 'orange'
+}
+
+function makeRed (evt) {
+  evt.target.style.backgroundColor = 'Red'
 }
 
 function makeWhite (evt) {
